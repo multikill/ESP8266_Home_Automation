de2e75a1c5775d31f6b2cd37cf98917893bd7ac2
multikill <balu_bear427@web.de>
2018-09-24 15:06:49 +0200

    First upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Home Automation Setup
+
+# Trigger Relay with Adafruit over IFTTT
+https://www.hackster.io/2stacks/alexa-trigger-esp8266-181f0d
+
+# Dash Button Clone 
+https://randomnerdtutorials.com/esp8266-wi-fi-button-diy-amazon-dash-button-clone/
+
+# Additionally
+MQTT Bibliothek von Adafruit
+ESP8266 Bibliothek
+Boardverwalter:
+http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
---
+++ dash_button/dash_button.ino
@@ -0,0 +1,116 @@
+// https://randomnerdtutorials.com/esp8266-wi-fi-button-diy-amazon-dash-button-clone/
+
+/*
+ * ESP8266 Wi-Fi Button
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+ */
+ 
+#include <ESP8266WiFi.h>
+
+//#define BUTTONPRESS0 0 // EPS8266 GPIO0
+//#define BUTTONPRESS2 2 // ESP8266 GPIO2
+
+// Replace with your SSID and Password
+const char* ssid     = "***";
+const char* password = "***";
+
+// Replace with your unique IFTTT URL resource
+const char* resource = "/trigger/esp01_button01/with/key/***";
+//char* resource0 = "/trigger/esp01_button00/with/key/***";
+//char* resource2 = "/trigger/esp01_button02/with/key/***";
+
+
+// How your resource variable should look like, but with your own API KEY (that API KEY below is just an example):
+//const char* resource = "/trigger/button_pressed/with/key/nAZjOphL3d-ZO4N3k64-1A7gTlNSrxMJdmqy";
+
+// Maker Webhooks IFTTT
+const char* server = "maker.ifttt.com";
+
+void setup() {
+  Serial.begin(115200); 
+
+//  pinMode(BUTTONPRESS0, INPUT);
+//  pinMode(BUTTONPRESS2, INPUT);
+//
+//  if(digitalRead(BUTTONPRESS0) == LOW) {
+//    //resource = resource0;
+//    Serial.println("BUTTON 00 was pressed");
+//  } 
+//  else if(digitalRead(BUTTONPRESS0) == LOW) {
+//    //resource = resource2;
+//    Serial.println("BUTTON 02 was pressed");
+//  }
+//  Serial.println(resource);
+
+  initWifi();
+  makeIFTTTRequest();
+
+  // Deep sleep mode until RESET pin is connected to a LOW signal (pushbutton is pressed)
+  ESP.deepSleep(0);
+}
+
+void loop() {
+  // sleeping so wont get here
+}
+
+// Establish a Wi-Fi connection with your router
+void initWifi() {
+  Serial.print("Connecting to: "); 
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);  
+
+  int timeout = 10 * 4; // 10 seconds
+  while(WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if(WiFi.status() != WL_CONNECTED) {
+     Serial.println("Failed to connect, going back to sleep");
+  }
+
+  Serial.print("WiFi connected in: "); 
+  Serial.print(millis());
+  Serial.print(", IP address: "); 
+  Serial.println(WiFi.localIP());
+}
+
+// Make an HTTP request to the IFTTT web service
+void makeIFTTTRequest() {
+  Serial.print("Connecting to "); 
+  Serial.print(server);
+  
+  WiFiClient client;
+  int retries = 5;
+  while(!!!client.connect(server, 80) && (retries-- > 0)) {
+    Serial.print(".");
+  }
+  Serial.println();
+  if(!!!client.connected()) {
+     Serial.println("Failed to connect, going back to sleep");
+  }
+  
+  Serial.print("Request resource: "); 
+  Serial.println(resource);
+  client.print(String("GET ") + resource + 
+                  " HTTP/1.1\r\n" +
+                  "Host: " + server + "\r\n" + 
+                  "Connection: close\r\n\r\n");
+                  
+  int timeout = 5 * 10; // 5 seconds             
+  while(!!!client.available() && (timeout-- > 0)){
+    delay(100);
+  }
+  if(!!!client.available()) {
+     Serial.println("No response, going back to sleep");
+  }
+  while(client.available()){
+    Serial.write(client.read());
+  }
+  
+  Serial.println("\nclosing connection");
+  client.stop();
+}
+
